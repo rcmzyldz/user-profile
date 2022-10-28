fad38e1cec95a441355c8f428e8d638324c80f78
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-10-28 10:01:32 +0200

    controller added

--- src/main/java/com/todo/userprofile/controller/UserController.java
+++ src/main/java/com/todo/userprofile/controller/UserController.java
@@ -1,6 +1,20 @@
 package com.todo.userprofile.controller;
 
+import com.todo.userprofile.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
+
+private final IUserService userService;
+
+
+
 
 
 }
